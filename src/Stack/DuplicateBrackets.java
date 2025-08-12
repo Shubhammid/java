0fcf929f5c44e5b76095badaf0e7e04b16ec2328
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-12 20:48:16 +0530

    Duplicate Brackets

---
+++ src/Stack/DuplicateBrackets.java
@@ -0,0 +1,33 @@
+package Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+//((a+b)+((c+d))
+//true
+
+public class DuplicateBrackets {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+
+        Stack<Character> st = new Stack<>();
+for(int i = 0; i< str.length(); i++) {
+    char ch = str.charAt(i);
+    if (ch == ')') {
+        if (st.peek() == '(') {
+            System.out.println("true");
+            return;
+        } else {
+            while(st.peek() != '(') {
+                st.pop();
+            }
+            st.pop();
+        }
+    } else {
+        st.push(ch);
+    }
+}
+        System.out.println("false");
+    }
+}
