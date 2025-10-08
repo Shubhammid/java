4dfbcac24f7ba99cdb7684f57defd64e9d7b817a
Shubham Midgule <shubham.midgule17@gmail.com>
2025-10-08 16:17:09 +0530

    First Commit

---
+++ src/Test/Demo_7.java
@@ -0,0 +1,18 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Demo_7 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int sp = n - 1;
+        int st = 1;
+
+
+        sp--;
+        st++;
+        System.out.println();
+    }
+}
---
+++ src/com/shubham/day1/Example.java
@@ -0,0 +1,13 @@
+package com.shubham.day1;
+
+public class Example {
+    void fun1(){
+        System.out.println("A");
+    }
+    void fun2(){
+        System.out.println("B");
+    }
+    public static void main(String[] args) {
+        System.out.println("C");
+    }
+}
