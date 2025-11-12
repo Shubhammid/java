5828d5c82b7b648f58cd2366326ff0bc9a081460
Shubham Midgule <shubham.midgule17@gmail.com>
2025-11-12 20:29:24 +0530

    Get Input from the User

---
+++ src/series/Day1.java
@@ -0,0 +1,18 @@
+package series;
+
+import java.util.Scanner;
+
+//1.Get Input from the User
+
+public class Day1 {
+    public static void main(String[] args) {
+        System.out.println("Enter the string");
+        Scanner sc =new Scanner(System.in);
+
+        String s1 = sc.nextLine();
+        System.out.println(s1);
+
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
