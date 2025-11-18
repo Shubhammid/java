cb9e2c57b76ad2c51428d2b56180579f46bbd9b1
Shubham Midgule <shubham.midgule17@gmail.com>
2025-11-18 21:52:19 +0530

    Java Program to Calculate Simple Interest

---
+++ src/series/Day7_2.java
@@ -0,0 +1,12 @@
+package series;
+
+//Java Program to Calculate Simple Interest
+
+public class Day7_2 {
+    public static void main(String[] args) {
+       float P = 1, R = 1, T = 1;
+
+       float SI = (P * R * T) / 100;
+        System.out.println("Simple interest = " + SI);
+    }
+}
