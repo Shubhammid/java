8a0aa4fe5454962503e0a2fe32fbd709894bc176
Shubham Midgule <shubham.midgule17@gmail.com>
2025-11-22 12:53:09 +0530

    Updated Number patter

--- src/series/Day10_1.java
+++ src/series/Day10_1.java
@@ -1,6 +1,10 @@
 package series;
 
 //Java Program to Print Pyramid Number Pattern
+//      1
+//    2 3 2
+//  3 4 5 4 3
+//4 5 6 7 6 5 4
 
 public class Day10_1 {
     public static void main(String[] args) {
