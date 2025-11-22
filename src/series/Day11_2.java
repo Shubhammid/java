2961ea4d10bcc4db7d34c585cd37279b90d5005c
Shubham Midgule <shubham.midgule17@gmail.com>
2025-11-22 13:04:12 +0530

    Java Program to Print Mirror Upper Star Triangle Pattern

---
+++ src/series/Day11_2.java
@@ -0,0 +1,28 @@
+package series;
+
+//Java Program to Print Mirror Upper Star Triangle Pattern
+
+public class Day11_2 {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 1; i <= n; i++) {
+            for (int s = n; s > i; s--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int s = n; s > i; s--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
