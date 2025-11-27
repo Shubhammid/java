730278bd02ad017f379e7bb404119a42e81a50c5
Shubham Midgule <shubham.midgule17@gmail.com>
2025-11-27 08:18:18 +0530

    Boolean toString() Method in Java

---
+++ src/series/Day15_2.java
@@ -0,0 +1,11 @@
+package series;
+
+//Boolean toString() Method in Java
+
+public class Day15_2 {
+    public static void main(String[] args) {
+        String str = "2033.12244";
+        double str1 = Double.parseDouble(str);
+        System.out.println(str1);
+    }
+}
