ab1bf7c8d968e8ad1ba98a0bd7053805a0d68f13
Shubham Midgule <shubham.midgule17@gmail.com>
2025-11-28 10:03:26 +0530

    Java Program to Convert String to Long

---
+++ src/series/Day16_2.java
@@ -0,0 +1,12 @@
+package series;
+
+//Java Program to Convert String to Long
+
+public class Day16_2 {
+    public static void main(String[] args) {
+        String str = "123456";
+        long num = Long.parseLong(str);
+        System.out.println("Long value " + num);
+        System.out.println("String value " + str);
+    }
+}
