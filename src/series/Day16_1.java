79316fc91df6ec2981fecb44d946f42d46f6ffb2
Shubham Midgule <shubham.midgule17@gmail.com>
2025-11-28 09:56:27 +0530

    Java Program to Convert Double to String

---
+++ src/series/Day16_1.java
@@ -0,0 +1,12 @@
+package series;
+
+//Java Program to Convert Double to String
+
+public class Day16_1 {
+    public static void main(String[] args) {
+        double num = 987.654;
+        String str = Double.toString(num);
+        System.out.println("Double value " + num);
+        System.out.println("String value " + str);
+    }
+}
