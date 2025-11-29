fe7e673006174ab7243c13bb9f73ca55b9edcfc8
Shubham Midgule <shubham.midgule17@gmail.com>
2025-11-29 11:06:13 +0530

    Java Program For Int to Char Conversion

---
+++ src/series/Day17_2.java
@@ -0,0 +1,11 @@
+package series;
+
+//Java Program For Int to Char Conversion
+
+public class Day17_2 {
+    public static void main(String[] args) {
+        int i = 97;
+        char ch = (char)i;
+        System.out.println(ch);
+    }
+}
