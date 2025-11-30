f61a37c55984d0b38fad59927a7abbf343feefac
Shubham Midgule <shubham.midgule17@gmail.com>
2025-11-30 11:13:55 +0530

    Java Program to Get a Character from a String

---
+++ src/series/Day18_1.java
@@ -0,0 +1,12 @@
+package series;
+
+//Java Program to Get a Character from a String
+
+public class Day18_1 {
+    public static void main(String[] args) {
+        String str = "ShubhamMidgule";
+        int index = 5;
+        char ch = str.charAt(index);
+        System.out.println("Character from " + str + " at index " + index+ " is " + ch);
+    }
+}
