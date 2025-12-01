f926852561a54038d5966c3063162abaeac3b755
Shubham Midgule <shubham.midgule17@gmail.com>
2025-12-01 18:27:08 +0530

    Java Program to Convert Char to Int

---
+++ src/series/Day19_2.java
@@ -0,0 +1,11 @@
+package series;
+
+//Java Program to Convert Char to Int
+
+public class Day19_2 {
+    public static void main(String[] args) {
+        char ch = '9';
+        int a = Integer.parseInt(String.valueOf(ch));
+        System.out.println("int value: " +a);
+    }
+}
