8f94a0635f5627dc03219eb9e2c84df046b7b1d0
Shubham Midgule <shubham.midgule17@gmail.com>
2025-12-03 10:50:49 +0530

    Java Program to Find Sum of Array Elements

---
+++ src/series/Day22_2.java
@@ -0,0 +1,17 @@
+package series;
+
+//Java Program to Find Sum of Array Elements
+
+public class Day22_2 {
+    static int arr[] = {12, 3, 4, 15};
+    static int sum(){
+        int sum = 0;
+        int i;
+        for(i = 0; i < arr.length; i++)
+            sum += arr[i];
+            return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("Sum of given array " + sum() );
+    }
+}
