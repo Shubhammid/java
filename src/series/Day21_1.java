ce7c03036bc0e362864b17b5345c21ab5966a226
Shubham Midgule <shubham.midgule17@gmail.com>
2025-12-03 10:45:52 +0530

    Java Program to Print Square Star Pattern

---
+++ src/series/Day21_1.java
@@ -0,0 +1,27 @@
+package series;
+
+//**********************
+//*                    *
+//*                    *
+//*                    *
+//*                    *
+//*                    *
+//*                    *
+//**********************
+public class Day21_1 {
+    static void print(int k, int l){
+        int a, b;
+        for(a = 1; a <= k; a++){
+            for(b = 1; b <= l; b++){
+                if( a == 1 || a == k || b == 1 || b == l)
+                    System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int rows = 8, columns = 22;
+        print(rows, columns);
+    }
+}
