e4615fcb4c40ddf436fde7bd01d331a25455c925
Shubham Midgule <shubham.midgule17@gmail.com>
2025-12-09 18:29:26 +0530

    Java program to print a Fibonacci sequence using recursion

---
+++ src/series/Day27_1.java
@@ -0,0 +1,21 @@
+package series;
+
+//Java program to print a Fibonacci sequence using recursion
+
+public class Day27_1 {
+    public static void printFibonacciSequence(int count){
+        int a = 0;
+        int b = 1;
+        int c = 1;
+
+        for(int i = 1; i <= count; i++){
+            System.out.print(a + ", ");
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    }
+    public static void main(String[] args) {
+        printFibonacciSequence(10);
+    }
+}
