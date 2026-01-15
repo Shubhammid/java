12e605805b295197796d5ddae1c5c4eafe441762
Shubham Midgule <shubham.midgule17@gmail.com>
2026-01-15 09:02:29 +0530

    Run length encoding

---
+++ src/series/Day51_1.java
@@ -0,0 +1,18 @@
+package series;
+
+public class Day51_1 {
+    public static void main(String[] args) {
+        String s = "aaabbc";
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i <= s.length(); i++) {
+            if (i < s.length() && s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                result.append(s.charAt(i - 1)).append(count);
+                count = 1;
+            }
+        }
+        System.out.println(result.toString());
+    }
+}
