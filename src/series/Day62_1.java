581cff879369ae43270e199e16a603a3221292b3
Shubham Midgule <shubham.midgule17@gmail.com>
2026-01-28 18:14:40 +0530

    Reverse linked list

---
+++ src/series/Day62_1.java
@@ -0,0 +1,41 @@
+package series;
+
+public class Day62_1 {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static Node reverseList(Node head) {
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    static void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        System.out.println("Original List:");
+        printList(head);
+        head = reverseList(head);
+        System.out.println("Reversed List:");
+        printList(head);
+    }
+}
