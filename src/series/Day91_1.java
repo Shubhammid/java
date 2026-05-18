169d84c583c0030afc365e18c89db95f8ddf6cfb
Shubham Midgule <shubham.midgule17@gmail.com>
2026-05-18 22:17:18 +0530

    Find max in sliding window

---
+++ src/series/Day91_1.java
@@ -0,0 +1,36 @@
+package series;
+
+import java.util.*;
+
+public class Day91_1 {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0 || k <= 0) {
+            return new int[0];
+        }
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            while (!deque.isEmpty() && deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int[] ans = maxSlidingWindow(arr, k);
+        System.out.println("Maximum in each sliding window:");
+        for (int num : ans) {
+            System.out.print(num + " ");
+        }
+    }
+}
