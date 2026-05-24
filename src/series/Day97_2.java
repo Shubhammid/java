7d02f3ee59d39d929a5bcf025f4c3990fd0c6c30
Shubham Midgule <shubham.midgule17@gmail.com>
2026-05-24 22:18:57 +0530

    Search in sorted matrix

---
+++ src/series/Day97_2.java
@@ -0,0 +1,20 @@
+package series;
+
+public class Day97_2 {
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        printMatrix(matrix);
+    }
+}
