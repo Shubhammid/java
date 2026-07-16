400bcb393614a09f23cb51efd52740181f558805
Shubham Midgule <shubham.midgule17@gmail.com>
2026-07-16 11:18:32 +0530

    Count set bits in number

---
+++ src/series/Day145_2.java
@@ -0,0 +1,16 @@
+package series;
+
+public class Day145_2 {
+    public static int countSetBits(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int n = 29;
+        System.out.println("Set Bits: " + countSetBits(n));
+    }
+}
