335e141913c45e0e746c39b4cead33a8b6aa2d4e
Shubham Midgule <shubham.midgule17@gmail.com>
2026-07-24 12:26:13 +0530

    Factorial Program in Java

---
+++ src/series/Day150_2.java
@@ -0,0 +1,17 @@
+package series;
+
+import java.util.Scanner;
+
+public class Day150_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial of " + num + " is: " + factorial);
+        sc.close();
+    }
+}
