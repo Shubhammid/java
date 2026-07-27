72a18315f56b833fd6037592131873d02b50207c
Shubham Midgule <shubham.midgule17@gmail.com>
2026-07-27 19:31:25 +0530

    How to Generate Random Number in Java

---
+++ src/series/Day151_2.java
@@ -0,0 +1,8 @@
+package series;
+
+public class Day151_2 {
+    public static void main(String[] args) {
+        int randomNumber = (int) (Math.random() * 100) + 1;
+        System.out.println("Random Number: " + randomNumber);
+    }
+}
